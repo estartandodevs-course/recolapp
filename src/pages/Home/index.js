bd1fbf1c0ff48cd4024d484f33e5b76cc2175a2b
Henrique Calenzo <calenzodevs@gmail.com>
2021-07-30 23:22:33 -0300

    feature/home
    	Components/Home
    		Added components required from page
    		Added styles in page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,35 @@
 import React from "react";
+
+import { useHistory } from "react-router-dom";
 import { Button } from "../../components/Button";
+// import { Input } from "../../components/Input";
+
+import * as S from "./style";
+
+import image from "../../assets/img/illustrations/tela_inicial.png";
 
 const Home = () => {
+  const history = useHistory();
+
   return (
     <>
-      <Button bgColor="#008000">Botão</Button>
-      <Button bgColor="#008000" disable>
-        Botão
-      </Button>
+      <S.Container>
+        <S.Title>Faça parte dessa iniciativa!</S.Title>
+        <S.Img src={image} alt="homeScreen" />
+        <S.ContainerBottom>
+          <Button
+            onClick={() => history.push("/register")}
+            bgColor="#008000"
+            width="85%"
+          >
+            Cadatrar
+          </Button>
+          <S.Pragraph>Já possuo cadastro</S.Pragraph>
+          <S.RedirectLogin onClick={() => history.push("/login")}>
+            Fazer login
+          </S.RedirectLogin>
+        </S.ContainerBottom>
+      </S.Container>
     </>
   );
 };
---
+++ src/pages/Home/style.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  color: #4f4f4f;
+`;
+export const Title = styled.h3`
+  padding: 32px 0px 0px 30px;
+`;
+export const Pragraph = styled.p`
+  padding: 8px 0px 4px 0px;
+  color: #4f4f4f;
+  font-size: 14px;
+`;
+export const RedirectLogin = styled.a`
+  color: #4f4f4f;
+  font-size: 14px;
+`;
+
+export const Img = styled.img`
+  padding: 0px 30px;
+`;
+
+export const ContainerBottom = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  padding-bottom: 32px;
+`;
