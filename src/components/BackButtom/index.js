39867ecd2f5ffbb7c6815894abd9da6b092c848a
Charles-504 <charles.com2010@gmail.com>
2021-07-31 15:25:00 -0300

    limpei comentarios

--- src/components/BackButtom/index.js
+++ src/components/BackButtom/index.js
@@ -5,9 +5,7 @@ import Backimg from "../../assets/img/icons/icon_arrow_left.svg";
 export const BackButton = ({ pageTitle, handleBack }) => {
   return (
     <S.Section>
-      {/* <S.Button> */}
       <S.Img src={Backimg} onClick={handleBack} />
-      {/* </S.Button> */}
       <S.Pagetitle>{pageTitle}</S.Pagetitle>
     </S.Section>
   );
