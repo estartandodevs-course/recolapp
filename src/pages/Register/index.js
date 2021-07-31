e971ee34c1598778ed2027d7ee01e55af13406c7
Fernanda Nardoni <thefray.br@gmail.com>
2021-07-31 14:52:52 -0300

    chore: estilizando página de registro WIP

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,10 +1,21 @@
-import { Container } from "./styles";
+import { Container, Wrapper } from "./styles";
 import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
 const Register = () => {
   return (
     <Container>
-      <Input id="name" value="name" label="Nome Completo" />
+      <Input id="name" value="" label="Nome Completo" />
+      <Input id="cpf" value="" label="CPF/CNPJ" />
+      <Input id="email" value="" label="E-mail" />
+      <Input id="telefone" value="" label="Telefone" />
+      <Input id="endereço" value="" label="Endereço" />
+      <Input id="cidade" value="" label="Cidade" />
+      <Wrapper>
+        <Input id="cep" value="" label="CEP" />
+        <Input id="estado" value="" label="Estado" />
+      </Wrapper>
+      <Button width="65%">Cadastrar</Button>
     </Container>
   );
 };
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -1,5 +1,21 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Wrapper = styled.div`
+  width: 60%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 50px;
+  margin-bottom: 10px;
 
 `;
