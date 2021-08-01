64e23eac6a109ebbc75fdb346475a28d24e6dbf4
Charles-504 <charles.com2010@gmail.com>
2021-07-31 23:26:36 -0300

    feat: componente select

---
+++ src/components/Select/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./styles";
+
+export const Select = () => {
+  return (
+    <S.Section>
+      <h1>teste</h1>
+    </S.Section>
+  );
+};
---
+++ src/components/Select/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Section = styled.div`
+  display: flex;
+  gap: 13px;
+  margin-left: 6.203%;
+  margin-top: 5.156%;
+`;
