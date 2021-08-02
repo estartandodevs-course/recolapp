c7adb048d28e9490797fc78a0aa185b67094f7eb
Fernanda Nardoni <thefray.br@gmail.com>
2021-08-02 20:12:03 -0300

    fix: problemas de formatação | chore: populando variável com info dos inputs

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,22 +1,87 @@
-import { Container, Wrapper } from "./styles";
+import { useState } from "react";
+import { FormContainer, Wrapper } from "./styles";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
 const Register = () => {
+  const [data, setData] = useState({
+  });
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
+
   return (
-    <Container>
-      <Input id="name" value="" label="Nome Completo" />
-      <Input id="cpf" value="" label="CPF/CNPJ" />
-      <Input id="email" value="" label="E-mail" />
-      <Input id="telefone" value="" label="Telefone" />
-      <Input id="endereço" value="" label="Endereço" />
-      <Input id="cidade" value="" label="Cidade" />
+    <FormContainer onSubmit={submit}>
+      <Input
+        id="name"
+        value={data.name}
+        onChange={(e) => setData({
+          ...data, name: e.target.value,
+        })}
+        label="Nome Completo"
+      />
+      <Input
+        id="cpf"
+        value={data.cpf}
+        onChange={(e) => setData({
+          ...data, cpf: e.target.value,
+        })}
+        label="CPF"
+      />
+      <Input
+        id="email"
+        value={data.emai}
+        onChange={(e) => setData({
+          ...data, email: e.target.value,
+        })}
+        label="E-mail"
+      />
+      <Input
+        id="telefone"
+        value={data.telefone}
+        onChange={(e) => setData({
+          ...data, telefone: e.target.value,
+        })}
+        label="Telefone"
+      />
+      <Input
+        id="endereco"
+        value={data.endereço}
+        onChange={(e) => setData({
+          ...data, endereco: e.target.value,
+        })}
+        label="Endereço"
+      />
+      <Input
+        id="cidade"
+        value={data.cidade}
+        onChange={(e) => setData({
+          ...data, cidade: e.target.value,
+        })}
+        label="Cidade"
+      />
       <Wrapper>
-        <Input id="cep" value="" label="CEP" />
-        <Input id="estado" value="" label="Estado" />
+        <Input
+          id="cep"
+          value={data.cep}
+          onChange={(e) => setData({
+            ...data, cep: e.target.value,
+          })}
+          label="CEP"
+        />
+        <Input
+          id="estado"
+          value={data.estado}
+          onChange={(e) => setData({
+            ...data, estado: e.target.value,
+          })}
+          label="Estado"
+        />
       </Wrapper>
-      <Button width="65%">Cadastrar</Button>
-    </Container>
+      <Button type="submit" width="65%">Cadastrar</Button>
+    </FormContainer>
   );
 };
 
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const FormContainer = styled.form`
   height: 100%;
   width: 100%;
 
