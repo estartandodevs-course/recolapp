62d90f15236e5954ae09c7696774f1c3b37f1764
lud-ic <mag.ludmila@gmail.com>
2021-08-03 18:38:18 -0300

    chore: addition the ...rest in the input mask component

--- src/components/InputMask/index.js
+++ src/components/InputMask/index.js
@@ -2,12 +2,12 @@ import React from "react";
 
 import * as S from "./styles";
 
-const Mask = ({ width = "300px", label, mask, value, onChange }) => {
+const Mask = ({ width = "300px", label, mask, value, onChange, ...rest }) => {
   return (
     <>
       <S.Container width={width}>
         <S.Label>{label}</S.Label>
-        <S.InputMask mask={mask} value={value} onChange={onChange} />
+        <S.InputMask mask={mask} value={value} onChange={onChange} {...rest} />
       </S.Container>
     </>
   );
