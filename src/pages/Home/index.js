35062c0d903dfc3a6db31bfd200c8eabf6c57570
Breno Nunes <brenontavares@gmail.com>
2021-08-03 21:30:26 -0300

    removendo comentarios da home + aplicando cor de variavel

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,32 +1,30 @@
 import React from "react";
 
-// import { useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
-// import { Button } from "../../components/Button";
+import { Button } from "../../components/Button";
 
-// import * as S from "./styles";
+import * as S from "./styles";
 
-// import image from "../../assets/img/illustrations/tela_inicial.svg";
-import { Selectb } from "../../components/Select";
+import image from "../../assets/img/illustrations/tela_inicial.svg";
 
 const Home = () => {
-  // const history = useHistory();
+  const history = useHistory();
 
   return (
-    <Selectb />
-    // <>
-    //   <S.Container>
-    //     <S.Title>Faça parte dessa iniciativa!</S.Title>
-    //     <S.Img src={image} alt="homeScreen" />
-    //     <S.ContainerBottom>
-    //       <Button onClick={() => history.push("/register")}>Cadatrar</Button>
-    //       <S.Pragraph>Já possuo cadastro</S.Pragraph>
-    //       <S.RedirectLogin onClick={() => history.push("/login")}>
-    //         Fazer login
-    //       </S.RedirectLogin>
-    //     </S.ContainerBottom>
-    //   </S.Container>
-    // </>
+    <>
+      <S.Container>
+        <S.Title>Faça parte dessa iniciativa!</S.Title>
+        <S.Img src={image} alt="homeScreen" />
+        <S.ContainerBottom>
+          <Button onClick={() => history.push("/register")}>Cadatrar</Button>
+          <S.Pragraph>Já possuo cadastro</S.Pragraph>
+          <S.RedirectLogin onClick={() => history.push("/login")}>
+            Fazer login
+          </S.RedirectLogin>
+        </S.ContainerBottom>
+      </S.Container>
+    </>
   );
 };
 
