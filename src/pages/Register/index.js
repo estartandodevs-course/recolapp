a614d5b765efab3ac63b2c69a9779fd15df8d830
lud-ic <mag.ludmila@gmail.com>
2021-08-04 10:46:12 -0300

    chore: WIP page register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,16 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as S from "./styles";
 import { Input } from "../../components/Input";
 import { BackButton } from "../../components/BackButtom";
 import { Button } from "../../components/Button";
+import { Mask } from "../../components/InputMask";
 
 const Register = () => {
-  const [data, setData] = useState({
-  });
+  const [data, setData] = useState({ cpf: "" });
+  var count = 0;
+  // const [cpfCnpj, setcpfCnpj] = useState("");
+  // useEffect(() => {
+  // const response =
+
+  //   setcpfCnpj(response);
+  // }, [data.cpf]);
 
   const submit = (e) => {
     e.preventDefault();
     console.log(data);
+  };
+
+  const getCpfAndCnpjMask = (value) => {
+    let onlyNumbers = value.replace(/[^\d]/g, "");
+    let length = onlyNumbers.length;
+    console.log(onlyNumbers);
+    console.log(count);
+    if (length > 11) {
+      console.log("teste a");
+      return "99.999.999/9999-99";
+    } else {
+      if (length > 10) {
+        count = count + 1;
+        console.log("teste c");
+      }
+      console.log("teste b");
+      return "999.999.999-99";
+    }
   };
 
   return (
@@ -20,71 +45,100 @@ const Register = () => {
         <Input
           id="name"
           value={data.name}
-          onChange={(e) => setData({
-            ...data, name: e.target.value,
-          })}
+          onChange={(e) =>
+            setData({
+              ...data,
+              name: e.target.value,
+            })
+          }
           label="Nome Completo"
           width="100%"
         />
-        <Input
+        <Mask
+          mask={getCpfAndCnpjMask(data.cpf)}
           id="cpf"
           value={data.cpf}
-          onChange={(e) => setData({
-            ...data, cpf: e.target.value,
-          })}
-          label="CPF"
+          onChange={(e) =>
+            setData({
+              ...data,
+              cpf: e.target.value,
+            })
+          }
+          label="CPF/CNPJ"
         />
         <Input
           id="email"
           value={data.emai}
-          onChange={(e) => setData({
-            ...data, email: e.target.value,
-          })}
+          onChange={(e) =>
+            setData({
+              ...data,
+              email: e.target.value,
+            })
+          }
           label="E-mail"
         />
-        <Input
-          id="telefone"
-          value={data.telefone}
-          onChange={(e) => setData({
-            ...data, telefone: e.target.value,
-          })}
-          label="Telefone"
+        <Mask
+          mask={"(99)99999-9999"}
+          id="celular"
+          value={data.celular}
+          onChange={(e) =>
+            setData({
+              ...data,
+              celular: e.target.value,
+            })
+          }
+          label="Celular"
         />
         <Input
           id="endereco"
           value={data.endereço}
-          onChange={(e) => setData({
-            ...data, endereco: e.target.value,
-          })}
+          onChange={(e) =>
+            setData({
+              ...data,
+              endereco: e.target.value,
+            })
+          }
           label="Endereço"
         />
         <Input
           id="cidade"
           value={data.cidade}
-          onChange={(e) => setData({
-            ...data, cidade: e.target.value,
-          })}
+          onChange={(e) =>
+            setData({
+              ...data,
+              cidade: e.target.value,
+            })
+          }
           label="Cidade"
         />
         <S.Wrapper>
-          <Input
+          <Mask
+            mask={"99999-999"}
             id="cep"
             value={data.cep}
-            onChange={(e) => setData({
-              ...data, cep: e.target.value,
-            })}
+            onChange={(e) =>
+              setData({
+                ...data,
+                cep: e.target.value,
+              })
+            }
             label="CEP"
           />
           <Input
             id="estado"
             value={data.estado}
-            onChange={(e) => setData({
-              ...data, estado: e.target.value,
-            })}
+            onChange={(e) =>
+              setData({
+                ...data,
+                estado: e.target.value,
+              })
+            }
             label="Estado"
           />
         </S.Wrapper>
-        <Button type="submit" width="100%">Cadastrar</Button>
+        <Button type="submit" width="100%">
+          Cadastrar
+        </Button>
       </S.FormContainer>
     </S.Container>
   );
