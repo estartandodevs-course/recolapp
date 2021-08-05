192b90097a6f5bea542a93448e9e4f0ed5400544
Charles-504 <charles.com2010@gmail.com>
2021-08-05 19:49:31 -0300

    chore:fixing styles select

--- src/components/Select/styles.js
+++ src/components/Select/styles.js
@@ -8,11 +8,20 @@ export const Select = styled(ReactSelect)`
   .css-yk16xz-control {
     background-color: var(--inputBackground);
     border-radius: 8px;
+    border: none;
   }
+
   .css-1okebmr-indicatorSeparator {
     display: none;
   }
+
   .css-1wa3eu0-placeholder {
     display: none;
+  }
+
+  .css-1pahdxg-control {
+    border: none;
+    box-shadow: none;
+    background-color: var(--inputBackground);
   }
 `;
