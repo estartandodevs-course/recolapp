adf5e32b03bdbd9c06ce32d75e09a7775934b4f7
lud-ic <mag.ludmila@gmail.com>
2021-08-04 22:48:23 -0300

    chore: ajusting select component

--- src/components/Select/styles.js
+++ src/components/Select/styles.js
@@ -8,11 +8,22 @@ export const Select = styled(ReactSelect)`
   .css-yk16xz-control {
     background-color: var(--inputBackground);
     border-radius: 8px;
+    border: none;
+    outline: none;
+    &:focus {
+      border: none;
+    }
   }
   .css-1okebmr-indicatorSeparator {
     display: none;
   }
   .css-1wa3eu0-placeholder {
     display: none;
+  }
+
+  .css-2b097c-container, .css-1f43avz-a11yText-A11yText, .css-1pahdxg-control {
+    outline: none;
+    border: none;
+    }
   }
 `;
