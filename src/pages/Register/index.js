931d107d3b9b8931674763e3ee1fe168c1852e61
Henrique Calenzo <calenzodevs@gmail.com>
2021-08-05 12:29:48 -0300

    feature/register
    	Pages/Register
    		Added SetState from get value
    		Added in data all value of the input from remove warning console
    		Change count of the varible from state
    		Remove width, not used

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as S from "./styles";
 import { Input } from "../../components/Input";
 import { BackButton } from "../../components/BackButtom";
@@ -6,32 +6,45 @@ import { Button } from "../../components/Button";
 import { Mask } from "../../components/InputMask";
 
 const Register = () => {
-  let count = 0;
-  const [data, setData] = useState({ cpf: "" });
+  let [count, setCount] = useState(1);
   const [maskCPF, SetMaskCPF] = useState("");
+  const [data, setData] = useState({
+    name: "", 
+    certification: "", 
+    email: "",
+    cellphone: "",
+    city: "",
+    street: "",
+    zip: "",
+    state: ""
+  });
 
   const submit = (e) => {
     e.preventDefault();
-    console.log(data);
   };
 
-  const getCpfAndCnpjMask = (value) => {
+  const getCertificationMask = (value) => {
     let onlyNumbers = value.replace(/[^\d]/g, "");
     let length = onlyNumbers.length;
 
     if (length === 11) {
-      count += 1;
+      setCount((count+1));
     }
 
     if (length >= 11 && count === 2) {
       SetMaskCPF("99.999.999/9999-99");
+      setCount(1)
     } else if (length <= 11) {
       SetMaskCPF("999.999.999-99");
     }
   };
 
-  const handleCpfCnpj = (e) => {
-    getCpfAndCnpjMask(e.target.value);
+  const handleCertification = (e) => {
+    setData({
+      ...data,
+      certification: e.target.value,
+    })
+    getCertificationMask(e.target.value);
   };
 
   return (
@@ -48,17 +61,17 @@ const Register = () => {
             })
           }
           label="Nome Completo"
-          width="100%"
         />
         <Mask
           mask={maskCPF}
           id="cpf"
-          onChange={(e) => handleCpfCnpj(e)}
+          value={data.certification}
+          onChange={(e) => handleCertification(e)}
           label="CPF/CNPJ"
         />
         <Input
           id="email"
-          value={data.emai}
+          value={data.email}
           onChange={(e) =>
             setData({
               ...data,
@@ -70,11 +83,11 @@ const Register = () => {
         <Mask
           mask={"(99)99999-9999"}
           id="celular"
-          value={data.celular}
+          value={data.cellphone}
           onChange={(e) =>
             setData({
               ...data,
-              celular: e.target.value,
+              cellphone: e.target.value,
             })
           }
           label="Celular"
@@ -85,18 +98,18 @@ const Register = () => {
           onChange={(e) =>
             setData({
               ...data,
-              endereco: e.target.value,
+              street: e.target.value,
             })
           }
           label="Endereço"
         />
         <Input
           id="cidade"
-          value={data.cidade}
+          value={data.city}
           onChange={(e) =>
             setData({
               ...data,
-              cidade: e.target.value,
+              city: e.target.value,
             })
           }
           label="Cidade"
@@ -105,22 +118,22 @@ const Register = () => {
           <Mask
             mask={"99999-999"}
             id="cep"
-            value={data.cep}
+            value={data.zip}
             onChange={(e) =>
               setData({
                 ...data,
-                cep: e.target.value,
+                zip: e.target.value,
               })
             }
             label="CEP"
           />
           <Input
             id="estado"
-            value={data.estado}
+            value={data.state}
             onChange={(e) =>
               setData({
                 ...data,
-                estado: e.target.value,
+                state: e.target.value,
               })
             }
             label="Estado"
