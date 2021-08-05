599f911eafa8396ad2c789e994134e2c2f59a283
lud-ic <mag.ludmila@gmail.com>
2021-08-05 11:15:56 -0300

    chore: ajusting select component

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import * as S from "./styles";
 
-export const Select = ({ options, onChange }) => {
-  return <S.Select options={options} onChange={onChange} />;
+export const Select = ({ label, width = "100%", options, onChange }) => {
+  return (
+    <>
+      <S.Container width={width}>
+        <S.Label>{label}</S.Label>
+        <S.Select options={options} onChange={onChange} />
+      </S.Container>
+    </>
+  );
 };
--- src/components/Select/styles.js
+++ src/components/Select/styles.js
@@ -1,9 +1,25 @@
 import styled from "styled-components";
 import ReactSelect from "react-select";
 
+export const Container = styled.div`
+  width: ${(props) => props.width};
+  height: 67px;
+  text-align: left;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+export const Label = styled.label`
+  color: #828282;
+  font-size: 12px;
+  line-height: 18px;
+`;
+
 export const Select = styled(ReactSelect)`
   width: 100%;
-  height: 45px;
+  /* height: 45px; */
 
   .css-yk16xz-control {
     background-color: var(--inputBackground);
