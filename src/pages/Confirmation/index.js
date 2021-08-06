8fab37dd825b15abe8e0ffe04271b0193cca50a9
Fernanda Nardoni <thefray.br@gmail.com>
2021-08-06 10:33:11 -0300

    feat: página de confirmação de agendamento

---
+++ src/pages/Confirmation/index.js
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+const Confirmation = () => {
+  return (
+    <S.PageContainer>
+      <S.ConfirmationText>Hello World</S.ConfirmationText>
+    </S.PageContainer>
+  );
+};
+
+export default Confirmation;
---
+++ src/pages/Confirmation/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const PageContainer = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const ConfirmationText = styled.h2`
+  text-align: center;
+`;
