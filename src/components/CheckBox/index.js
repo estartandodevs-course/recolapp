21a05d74737c61f7e33f0a59ce66f8f7f9633593
lud-ic <mag.ludmila@gmail.com>
2021-08-07 16:45:25 -0300

    chore: ajusting checkbox component to use in Register pages

--- src/components/CheckBox/index.js
+++ src/components/CheckBox/index.js
@@ -1,18 +1,32 @@
+// import * as S from "./styles";
 import React from "react";
-import Checkbox from "@material-ui/core/Checkbox";
-import * as S from "./styles";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { withStyles } from "@material-ui/core/styles";
+import CheckboxLib from "@material-ui/core/Checkbox";
 
 const Checkbox = ({ label, checked, onChange, inputProps, ...rest }) => {
+  const GreenCheckbox = withStyles({
+    root: {
+      color: "var(--textColor)",
+      "&$checked": {
+        color: "var(--textColor)",
+      },
+    },
+    checked: {},
+  })((props) => <CheckboxLib color="default" {...props} />);
+
   return (
     <>
-      <S.CheckboxContainer {...rest}>
-        <S.Checkbox
-          checked={checked}
-          onChange={onChange}
-          inputProps={inputProps}
-          label={label}
-        />
-      </S.CheckboxContainer>
+      <FormControlLabel
+        control={
+          <GreenCheckbox
+            checked={checked}
+            onChange={onChange}
+            inputProps={inputProps}
+          />
+        }
+        label={label}
+      />
     </>
   );
 };
