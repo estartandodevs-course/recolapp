b0e733db5d6fdbc39c75d949a2391bb9be286756
lud-ic <mag.ludmila@gmail.com>
2021-08-07 12:06:14 -0300

    chore: modify checkbox into to a lib

--- src/components/CheckBox/index.js
+++ src/components/CheckBox/index.js
@@ -1,12 +1,17 @@
 import React from "react";
+import Checkbox from "@material-ui/core/Checkbox";
 import * as S from "./styles";
 
-const Checkbox = ({ label, type = "checkboox", id, onChange, ...rest }) => {
+const Checkbox = ({ label, checked, onChange, inputProps, ...rest }) => {
   return (
     <>
       <S.CheckboxContainer {...rest}>
-        <S.InputCheck type={type} id={id}></S.InputCheck>
-        <S.Label>{label}</S.Label>
+        <S.Checkbox
+          checked={checked}
+          onChange={onChange}
+          inputProps={inputProps}
+          label={label}
+        />
       </S.CheckboxContainer>
     </>
   );
