0dea1bacc0ae54c8fac346abe4e03f5efa4a36ed
lud-ic <mag.ludmila@gmail.com>
2021-08-08 01:18:15 -0300

    Revert "chore: ajusting checkbox component to use in Register pages"
    This reverts commit 21a05d74737c61f7e33f0a59ce66f8f7f9633593.

--- src/components/CheckBox/index.js
+++ src/components/CheckBox/index.js
@@ -1,33 +1,18 @@
-// import * as S from "./styles";
 import React from "react";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import { withStyles } from "@material-ui/core/styles";
-import CheckboxLib from "@material-ui/core/Checkbox";
+import Checkbox from "@material-ui/core/Checkbox";
+import * as S from "./styles";
 
 const Checkbox = ({ label, checked, onChange, inputProps, ...rest }) => {
-  const GreenCheckbox = withStyles({
-    root: {
-      color: "var(--textColor)",
-      "&$checked": {
-        color: "var(--textColor)",
-      },
-    },
-    checked: {},
-  })((props) => <CheckboxLib color="default" {...props} />);
-
   return (
     <>
-      <FormControlLabel
-        control={
-          <GreenCheckbox
-            checked={checked}
-            onChange={onChange}
-            inputProps={inputProps}
-          />
-        }
-        label={label}
-        {...rest}
-      />
+      <S.CheckboxContainer {...rest}>
+        <S.Checkbox
+          checked={checked}
+          onChange={onChange}
+          inputProps={inputProps}
+          label={label}
+        />
+      </S.CheckboxContainer>
     </>
   );
 };
--- src/components/CheckBox/styles.js
+++ src/components/CheckBox/styles.js
@@ -1,18 +1,17 @@
 import styled from "styled-components";
-// import checkbox from "@material-ui/core/Checkbox";
 
-// export const CheckboxContainer = styled.div`
-//   display: flex;
-//   align-items: center;
-// `;
+export const CheckboxContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
-// export const CheckboxLib = styled(checkbox)`
-//   width: 12px;
-//   height: 12px;
-// `;
+export const Checkbox = styled.input`
+  width: 12px;
+  height: 12px;
+`;
 
-// export const Label = styled.label`
-//   color: var(--textColor);
-//   font-size: 15px;
-//   margin-left: 10px;
-// `;
+export const Label = styled.label`
+  color: var(--textColor);
+  font-size: 15px;
+  margin-left: 10px;
+`;
