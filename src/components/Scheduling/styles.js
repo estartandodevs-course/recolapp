5bcea2570efc5c893f4caa3839558ef594debf94
Henrique Calenzo <calenzodevs@gmail.com>
2021-08-10 13:46:22 -0300

    refactor/scheduling
    	Scheduling/Styles
    		Added font-size and color

--- src/components/Scheduling/styles.js
+++ src/components/Scheduling/styles.js
@@ -5,7 +5,6 @@ export const SchedulingContainer = styled.div`
   width: 100%;
   height: 100px;
   border-radius: 8px;
-  font-family: Poppins;
 
   display: flex;
   align-items: flex-start;
@@ -14,12 +13,15 @@ export const SchedulingContainer = styled.div`
 `;
 
 export const SchedulingDiv = styled.div`
-  /* width: calc(100% - 12px); */
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: row;
   padding-left: 12px;
+
+  color: #000000;
+  /* font-family: "Poppins400"; */
+  font-size: 16px;
 `;
 
 export const SchedulingKey = styled.h3``;
