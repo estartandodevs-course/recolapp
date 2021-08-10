81cc77f44b0b1c10c0fd083b779b214c59007ea4
lud-ic <mag.ludmila@gmail.com>
2021-08-10 14:41:49 -0300

    chore: addition of a index.js file to import the icons

---
+++ src/assets/img/icons/index.js
@@ -0,0 +1,15 @@
+import homeActive from "./home-active.svg";
+import homeInactive from "./home-inactive.svg";
+import qaActive from "./qa-active.svg";
+import qaInactive from "./qa-inactive.svg";
+import profileActive from "./profile-active.svg";
+import profileInactive from "./profile-inactive.svg";
+
+export {
+  homeActive,
+  homeInactive,
+  qaActive,
+  qaInactive,
+  profileActive,
+  profileInactive,
+};
