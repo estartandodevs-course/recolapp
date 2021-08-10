a04aa768d1368291dd02bed5ed74470c53c1c3f6
Henrique Calenzo <calenzodevs@gmail.com>
2021-08-10 17:34:41 -0300

    fix/selectMaterial
    	Componente/SelectMaterial
    		Refactor function the of change color button clicked

--- src/components/SelectMaterial/index.js
+++ src/components/SelectMaterial/index.js
@@ -1,38 +1,63 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 
 import * as S from "./styles";
 
-const SelectMaterial = ({ array, ...rest }) => {
+const SelectMaterial = ({
+  array = [
+    {
+      name: "Papel",
+      status: true,
+    },
+    {
+      name: "Papelão",
+      status: false,
+    },
+    {
+      name: "Plástico",
+      status: false,
+    },
+    {
+      name: "Alumínio",
+      status: false,
+    },
+    {
+      name: "Vidro",
+      status: false,
+    },
+    {
+      name: "Metal",
+      status: false,
+    },
+  ],
+  ...rest
+}) => {
   const [material, setMaterial] = useState(array);
 
-  useEffect(() => {
-    console.log(material);
-    setMaterial({ ...material });
-  }, [material]);
+  const onClick = (element) => {
+    setMaterial((previous) => {
+      return previous.map((item) => {
+        if (item.name == element.name) {
+          return { ...item, status: !item.status };
+        }
+        return item;
+      });
+    });
+  };
 
   return (
-    <>
-      <S.ContainerSelectMaterial {...rest}>
-        {material &&
-          material.map((element) => {
-            return (
-              <>
-                <S.ContainerMaterial
-                  key={`Material${element.id}`}
-                  status={element.status}
-                  onClick={() =>
-                    (element.status = element.status ? false : true)
-                  }
-                >
-                  <S.ContainerMaterialText>
-                    {element?.name}
-                  </S.ContainerMaterialText>
-                </S.ContainerMaterial>
-              </>
-            );
-          })}
-      </S.ContainerSelectMaterial>
-    </>
+    <S.ContainerSelectMaterial {...rest}>
+      {material?.map((element) => {
+        return (
+          <S.ContainerMaterial
+            key={element.name}
+            status={element.status}
+            onClick={() => onClick(element)}
+          >
+            <S.ContainerMaterialText>{element?.name}</S.ContainerMaterialText>
+          </S.ContainerMaterial>
+        );
+      })}
+    </S.ContainerSelectMaterial>
   );
 };
 
