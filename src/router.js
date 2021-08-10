01436822c8da29f700eb5653376dce5178e515c8
Henrique Calenzo <calenzodevs@gmail.com>
2021-08-10 20:55:12 -0300

    fix: conflicts

---
+++ src/router.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import * as R from "./pages";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={R.Home} />
+        <Route exact path="/login" component={R.Login} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
