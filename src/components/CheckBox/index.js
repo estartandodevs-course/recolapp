c4766336cfd9c6732e9a176746c601d3a0092930
Henrique Calenzo <calenzodevs@gmail.com>
2021-08-12 18:07:11 -0300

    fix/register
    	.eslintrc
    		Added "no-restricted-syntax" from ignore syntax For
    
    	Components/Checkbox
    		Disable eslint this component
    
    	Pages/Register
    		Remove warning eslint
    
    	Pages/Index and src/routes
    		Added page Register

--- src/components/CheckBox/index.js
+++ src/components/CheckBox/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 // import * as S from "./styles";
 import React from "react";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Home from "./Home";
 import Login from "./Login";
+import Register from "./Register";
 import HomeE from "./HomeEmpreeendedor";
 
-export { Home, Login, HomeE };
+export { Home, Login, HomeE, Register };
