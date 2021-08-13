cadec8d7e1f8b8a0c143aae7cfe3e1347dae33de
Ludmila Magalhães <82523274+Lud-ic@users.noreply.github.com>
2021-08-13 03:05:13 -0300

    Delete index.js

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Home from "./Home";
-import Login from "./Login";
-import Register from "./Register";
-import HomeE from "./HomeEmpreeendedor";
-import DetailsScheduling from "./DetailsScheduling";
-
-export { Home, Login, HomeE, Register, DetailsScheduling };
