c57e96fb77413070b99f12d6790bcc673b48bd98
Fernanda Nardoni <thefray.br@gmail.com>
2021-08-13 00:57:44 -0300

    chore: página de listagem de agendamentos WIP

---
+++ src/pages/ScheduledCollections/index.js
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+const ScheduledCollections = () => {
+  return (
+    <S.CollectionsContainer>
+      <h1>Hello World</h1>
+    </S.CollectionsContainer>
+  );
+};
+
+export default ScheduledCollections;
---
+++ src/pages/ScheduledCollections/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const CollectionsContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 30px;
+`;
