fe6cdd8102ac9d3b1ef53da6f2ad3b7508793bb0
lud-ic <mag.ludmila@gmail.com>
2021-08-13 21:35:50 -0300

    fix: styles page

--- src/pages/ScheduledCollections/index.js
+++ src/pages/ScheduledCollections/index.js
@@ -1,16 +1,18 @@
 import * as S from "./styles";
 import myCollections from "../../assets/img/illustrations/meus_agendamentos.svg";
-import { ViewDetails } from "../../components/ViewDetails";
 import { TabBar } from "../../components/TabBar";
 
 const ScheduledCollections = () => {
   return (
-    <S.CollectionsContainer>
-      <S.CollectionsButton pageTitle="Meus agendamentos" />
-      <S.CollectionsImg src={myCollections} />
-      <ViewDetails />
+    <>
+      <S.CollectionsContainer>
+        <S.CollectionsButton pageTitle="Meus agendamentos" />
+        <S.CollectionsImg src={myCollections} />
+        <S.ViewSettings />
+        <S.ViewSettings />
+      </S.CollectionsContainer>
       <TabBar />
-    </S.CollectionsContainer>
+    </>
   );
 };
 
--- src/pages/ScheduledCollections/styles.js
+++ src/pages/ScheduledCollections/styles.js
@@ -1,20 +1,24 @@
 import styled from "styled-components";
 import { BackButton } from "../../components/BackButtom";
+import { ViewDetails } from "../../components/ViewDetails";
 
 export const CollectionsContainer = styled.div`
-  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  margin: 0 30px 80px;
+  margin: 0 30px;
 `;
 
 export const CollectionsButton = styled(BackButton)`
   margin-top: 20px;
   margin-right: auto;
-  position: fixed;
+`;
+
+export const ViewSettings = styled(ViewDetails)`
+  margin-bottom: 48px;
 `;
 
 export const CollectionsImg = styled.img`
   margin: auto;
+  margin-bottom: 41px;
 `;
