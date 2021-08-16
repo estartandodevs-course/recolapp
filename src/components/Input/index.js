6272cc0a6d431ca412dfb1c8b9851a6941577107
lud-ic <mag.ludmila@gmail.com>
2021-08-15 23:10:49 -0300

    chore: adjusted Input component, set props to backgroundcolor and label color

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -7,6 +7,8 @@ const Input = ({
   value,
   label,
   width = "100%",
+  bgColor = "#fde5d7",
+  labelColor = "#828282",
   ...rest
 }) => {
   return (
@@ -19,6 +21,8 @@ const Input = ({
           value={value}
           onChange={onChange}
           width={width}
+          bgColor={bgColor}
+          labelColor={labelColor}
         />
       </S.Container>
     </>
