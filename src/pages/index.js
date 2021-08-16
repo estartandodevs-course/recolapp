76b9805f6c728e0c47ab2965cc9109fffe2b73b0
lud-ic <mag.ludmila@gmail.com>
2021-08-16 19:17:45 -0300

    import collection confirm and collection cancelled pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,16 @@ import Register from "./Register";
 import HomeEntrepreneur from "./HomeEntrepreneur";
 import SchedulesDetails from "./SchedulesDetails";
 import Schedules from "./Schedules";
+import CollectionConfirm from "./CollectionConfirm";
+import CollectionCancelled from "./CollectionCancelled";
 
-export { Home, Login, HomeEntrepreneur, Register, Schedules, SchedulesDetails };
+export {
+  Home,
+  Login,
+  HomeEntrepreneur,
+  Register,
+  Schedules,
+  SchedulesDetails,
+  CollectionConfirm,
+  CollectionCancelled,
+};
