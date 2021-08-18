3e88226691ee130cd19666e0f1b668f492d37046
lud-ic <mag.ludmila@gmail.com>
2021-08-18 02:55:53 -0300

    chore: WIP change register page to reponsive

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -82,99 +82,109 @@ const Register = () => {
   }, [data]);
 
   return (
-    <S.Container>
-      <S.BackButtonRegister pageTitle="Faça seu cadastro" />
-      <S.FormContainer>
-        <S.InputRegister
-          id="name"
-          value={data.name}
-          onChange={(e) => setData({ ...data, name: e.target.value })}
-          label="Nome Completo"
-        />
-        <S.MaskCertification
-          mask={maskCertification}
-          id="cpf"
-          value={data.certification}
-          onChange={(event) => handleCertification(event)}
-          label="CPF/CNPJ"
-        />
-        <S.InputRegister
-          id="email"
-          value={data.email}
-          onChange={(e) => setData({ ...data, email: e.target.value })}
-          label="E-mail"
-        />
-        <S.InputRegister
-          id="password1"
-          value={password.one}
-          onChange={(e) => SetPassword({ ...password, one: e.target.value })}
-          label="Senha"
-          type="password"
-        />
-        <S.InputRegister
-          id="password2"
-          value={password.two}
-          onChange={(e) => SetPassword({ ...password, two: e.target.value })}
-          label="Confirmar Senha"
-          type="password"
-        />
-        <S.MaksCellphone
-          mask="(99)99999-9999"
-          id="celular"
-          value={data.cellphone}
-          onChange={(e) => setData({ ...data, cellphone: removeMask(e.target.value) })}
-          label="Celular"
-        />
-        <S.InputRegister
-          id="endereco"
-          value={data.street}
-          onChange={(e) => setData({ ...data, street: e.target.value })}
-          label="Endereço"
-        />
-        <S.InputRegister
-          id="cidade"
-          value={data.city}
-          onChange={(e) => setData({ ...data, city: e.target.value })}
-          label="Cidade"
-        />
-        <S.Wrapper>
-          <S.MaskCep
-            mask="99999-999"
-            id="cep"
-            value={data.zip}
-            onChange={(e) => setData({ ...data, zip: removeMask(e.target.value) })}
-            label="CEP"
+    <>
+      <S.HeaderWebPage />
+      <S.Container>
+        <S.BackButtonRegister pageTitle="Faça seu cadastro" />
+        <S.RegisterTitle>Faça seu cadastro</S.RegisterTitle>
+        <S.FormContainer>
+          <S.InputRegister
+            id="name"
+            value={data.name}
+            onChange={(e) => setData({ ...data, name: e.target.value })}
+            label="Nome Completo"
           />
-          <S.SelectStates
-            id="estado"
-            value={data.state}
-            onChange={(e) => setData({ ...data, state: e.value })}
-            label="Estado"
-            options={States}
+          <S.MaskCertification
+            mask={maskCertification}
+            id="cpf"
+            value={data.certification}
+            onChange={(event) => handleCertification(event)}
+            label="CPF/CNPJ"
           />
-        </S.Wrapper>
-        <S.SelectTypeUser
-          id="typeUser"
-          value={data.typeUser}
-          onChange={(e) => setData({ ...data, typeUser: e.value })}
-          label="Você é:"
-          options={typeUser}
-        />
-      </S.FormContainer>
-      <S.CheckboxRegister
-        label="Li e concordo com os termos de uso"
-        checked={data.boxCheck}
-        onChange={(e) => setData({ ...data, boxCheck: e.target.checked })}
-      />
+          <S.InputRegister
+            id="email"
+            value={data.email}
+            onChange={(e) => setData({ ...data, email: e.target.value })}
+            label="E-mail"
+          />
+          <S.InputRegister
+            id="password1"
+            value={password.one}
+            onChange={(e) => SetPassword({ ...password, one: e.target.value })}
+            label="Senha"
+            type="password"
+          />
+          <S.InputRegister
+            id="password2"
+            value={password.two}
+            onChange={(e) => SetPassword({ ...password, two: e.target.value })}
+            label="Confirmar Senha"
+            type="password"
+          />
+          <S.MaksCellphone
+            mask="(99)99999-9999"
+            id="celular"
+            value={data.cellphone}
+            onChange={(e) =>
+              setData({ ...data, cellphone: removeMask(e.target.value) })
+            }
+            label="Celular"
+          />
+          <S.InputRegister
+            id="endereco"
+            value={data.street}
+            onChange={(e) => setData({ ...data, street: e.target.value })}
+            label="Endereço"
+          />
+          <S.InputRegister
+            id="cidade"
+            value={data.city}
+            onChange={(e) => setData({ ...data, city: e.target.value })}
+            label="Cidade"
+          />
+          <S.Wrapper>
+            <S.MaskCep
+              mask="99999-999"
+              id="cep"
+              value={data.zip}
+              onChange={(e) =>
+                setData({ ...data, zip: removeMask(e.target.value) })
+              }
+              label="CEP"
+            />
+            <S.SelectStates
+              id="estado"
+              value={data.state}
+              onChange={(e) => setData({ ...data, state: e.value })}
+              label="Estado"
+              options={States}
+            />
+          </S.Wrapper>
+          <S.SelectTypeUser
+            id="typeUser"
+            value={data.typeUser}
+            onChange={(e) => setData({ ...data, typeUser: e.value })}
+            label="Você é:"
+            options={typeUser}
+          />
+        </S.FormContainer>
+        <S.CheckButton>
+          <S.CheckboxRegister
+            label="Li e concordo com os termos de uso"
+            checked={data.boxCheck}
+            onChange={(e) => setData({ ...data, boxCheck: e.target.checked })}
+          />
 
-      <S.ErrorMessage disable={errorRegister.status}>
-        {errorRegister.message}
-      </S.ErrorMessage>
+          <S.ErrorMessage disable={errorRegister.status}>
+            {errorRegister.message}
+          </S.ErrorMessage>
 
-      <S.ButtonRegister disable={disable} onClick={() => finishRegister()}>
-        Finalizar cadastro
-      </S.ButtonRegister>
-    </S.Container>
+          <S.ButtonRegister disable={disable} onClick={() => finishRegister()}>
+            Finalizar cadastro
+          </S.ButtonRegister>
+        </S.CheckButton>
+      </S.Container>
+    </>
   );
 };
 
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -6,30 +6,70 @@ import { Input } from "../../components/Input";
 import { Select } from "../../components/Select";
 import { Button } from "../../components/Button";
 import { Checkbox } from "../../components/CheckBox";
+import { HeaderWeb } from "../../components/HeaderWeb";
+
+export const HeaderWebPage = styled(HeaderWeb)`
+  display: none;
+  @media (min-width: 768px) {
+    display: flex;
+    position: relative;
+    z-index: 1;
+  }
+`;
 
 export const Container = styled.div`
   background-color: var(--lightGray);
   color: var(--textColor);
   padding: 0px 30px;
 
-  display: flex;
+  /* display: flex; */
   justify-content: center;
   flex-direction: column;
+
+  @media (min-width: 768px) {
+    width: 808px;
+    margin: auto;
+  }
 `;
 
 export const BackButtonRegister = styled(BackButton)`
   margin-top: 33px;
   margin-left: 0;
   margin-bottom: 25px;
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const RegisterTitle = styled.h1`
+  display: none;
+
+  @media (min-width: 768px) {
+    display: flex;
+    font-size: 43px;
+    color: var(--textColor);
+    margin: 98px 0 65px 0;
+  }
 `;
 
 export const FormContainer = styled.form`
   height: 100%;
+  @media (min-width: 768px) {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 24px;
+  }
 `;
 
 export const InputRegister = styled(Input)`
   gap: 4px;
   padding-bottom: 16px;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    /* height: 59px; */
+  }
 `;
 
 export const MaskCertification = styled(Mask)`
@@ -61,13 +101,31 @@ export const SelectTypeUser = styled(Select)`
   margin-bottom: 16.5px;
 `;
 
+export const CheckButton = styled.div`
+  @media (min-width: 768px) {
+    width: 600px;
+    margin: auto;
+    margin-top: 240px;
+  }
+`;
+
 export const CheckboxRegister = styled(Checkbox)`
   margin-top: 16.5px;
   margin-bottom: 20px;
+
+  @media (min-width: 768px) {
+    font-size: 18px;
+  }
 `;
 
 export const ButtonRegister = styled(Button)`
   margin-bottom: 51px;
+
+  @media (min-width: 768px) {
+    border-radius: 18px;
+    height: 102px;
+    font-size: 32px;
+  }
 `;
 
 export const ErrorMessage = styled.p`
