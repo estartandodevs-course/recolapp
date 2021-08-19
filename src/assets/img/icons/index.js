fad8524c7ab2be916c64b581b70a741a6c7d9a89
lud-ic <mag.ludmila@gmail.com>
2021-08-19 03:30:53 -0300

    added icons used in the profile page

--- src/assets/img/icons/index.js
+++ src/assets/img/icons/index.js
@@ -7,6 +7,11 @@ import profileInactive from "./profile-inactive.svg";
 import checkMark from "./check-mark.svg";
 import location from "./location.svg";
 import recicle from "./recicle.svg";
+import profileUser from "./profile-user.svg";
+import verify from "./verify.svg";
+import edit from "./edit.svg";
+import star from "./star.svg";
+import whatsapp from "./whatsapp.svg";
 
 export {
   homeActive,
@@ -18,4 +23,9 @@ export {
   checkMark,
   location,
   recicle,
+  profileUser,
+  verify,
+  edit,
+  star,
+  whatsapp,
 };
--- src/assets/img/illustrations/index.js
+++ src/assets/img/illustrations/index.js
@@ -3,5 +3,13 @@ import myCollections from "./meus_agendamentos.svg";
 import collectionConfirmation from "./coleta_sucesso.svg";
 import sentMessage from "./mensagem_enviada.svg";
 import homeWeb from "./home-web.svg";
+import profile from "./perfil_coletor.svg";
 
-export { faq, myCollections, collectionConfirmation, sentMessage, homeWeb };
+export {
+  faq,
+  myCollections,
+  collectionConfirmation,
+  sentMessage,
+  homeWeb,
+  profile,
+};
