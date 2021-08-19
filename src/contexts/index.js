b8861f7c853dbc7c25b636a15954e9f1def5ece6
Henrique Calenzo <calenzodevs@gmail.com>
2021-08-19 01:04:59 -0300

    Added new states in context

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -5,9 +5,19 @@ const UserContext = createContext();
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState({});
   const [order, setOrder] = useState([]);
+  const [orderTimestamp, setOrderTimestamp] = useState("");
 
   return (
-    <UserContext.Provider value={{ user, setUser, order, setOrder }}>
+    <UserContext.Provider
+      value={{
+        user,
+        setUser,
+        order,
+        setOrder,
+        orderTimestamp,
+        setOrderTimestamp,
+      }}
+    >
       {children}
     </UserContext.Provider>
   );
